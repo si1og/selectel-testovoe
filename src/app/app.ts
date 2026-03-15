b87574f4d94bc852b57c49d21f4fe415a291eda0
Ilya Semenov <hi@si1og.ru>
2026-03-15 17:19:07 +0300

    add menu items state service; add items loading from the json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuStateService, MenuItem } from './core/services/menu-state.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('ilya-semenov-selectel-testovoe');
+  constructor(
+    private http: HttpClient,
+    private menuStateService: MenuStateService,
+  ) {
+    this.http.get<MenuItem[]>('assets/data/menu-items.json').subscribe((items) => {
+      this.menuStateService.setItems(items);
+      this.menuStateService.showItems();
+    });
+  }
 }
--- src/app/core/services/menu-state.service.ts
+++ src/app/core/services/menu-state.service.ts
@@ -1,8 +1,40 @@
 import { Injectable, signal, computed } from '@angular/core';
 
-interface MenuItem {
+export interface MenuItem {
   id: number;
   name: string;
   value: number;
   selected: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
+export class MenuStateService {
+  private items = signal<MenuItem[]>([]);
+
+  setItems(items: MenuItem[]) {
+    this.items.set(items);
+  }
+
+  toggleItem(id: number) {
+    this.items.update((items) =>
+      items.map((item) => (item.id === id ? { ...item, selected: !item.selected } : item)),
+    );
+  }
+
+  readonly selectedItemsNumber = computed(() => {
+    return this.items().filter((item) => item.selected).length;
+  });
+
+  readonly selectedItemsValue = computed(() => {
+    return this.items()
+      .filter((item) => item.selected)
+      .reduce((sum, item) => sum + item.value, 0);
+  });
+
+  showItems() {
+    console.log('Items in the state service:');
+    this.items().forEach((item) => {
+      console.log(item);
+    });
+  }
 }
